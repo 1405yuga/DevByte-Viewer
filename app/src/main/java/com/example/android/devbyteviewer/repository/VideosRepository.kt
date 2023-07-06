17dd6f991906f585437e5234023e9f78db6b57a0
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-07-06 09:29:19 +0530

    created function and setup coroutine

--- app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,7 +16,24 @@
 
 package com.example.android.devbyteviewer.repository
 
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 /**
  * Repository for fetching devbyte videos from the network and storing them on disk
  */
-// TODO: Implement the VideosRepository class
+//  Implement the VideosRepository class
+
+class VideosRepository(private  val database : VideosDatabase){
+
+    //used to refesh offline cached data
+    suspend fun refreshData(){
+        withContext(Dispatchers.IO){
+            val playlists = DevByteNetwork.devbytes.getPlaylist()
+        }
+
+    }
+
+}
